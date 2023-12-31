7a0506cc365b8ff4abcdf439f4f9d9b0ae687341
Eghosa-Osayande <osayandeeghosam@gmail.com>
2023-12-31 20:50:28 +0100

    edit docker compose

--- cmds.sh
+++ cmds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 alias air=$(go env GOPATH)/bin/air&&alias swag=./swag
 
-goose postgres postgres://root:root@localhost:5432/localdevdb up
+goose -dir "sql/schema" postgres postgres://root:root@localhost:5432/localdevdb up
